e743183e3b75910da3a22e7ee75fbae036c7985c
AimrayX <timothy.hall@gmx.ch>
2025-08-02 13:31:03 +0200

    wip - started writing definitions

--- ads1015.h
+++ ads1015.h
@@ -31,7 +31,76 @@
 #ifndef ADS1015_H
 #define ADS1015_H
 
+typedef enum ads1015_conv_status_e{
+    ADS1015_CONV_NOT_BUSY = 0,
+    ADS1015_CONV_BUSY     = 1,
 
+} ads1015_conv_status_t;
 
+typedef enum ads1015_conv_command_e{
+    ADS1015_CONV_NO_OP = 0,
+    ADS1015_CONV_START = 1,
+
+} ads1015_conv_command_t;
+
+typedef enum ads1015_mux_e{
+    ADS1015_MUX_AIN0_AIN1    = 0,
+    ADS1015_MUX_AIN0_AIN3    = 1,
+    ADS1015_MUX_AIN1_AIN3    = 2,
+    ADS1015_MUX_AIN2_AIN3    = 3,
+    ADS1015_MUX_AIN0_AIN_GND = 4,
+    ADS1015_MUX_AIN1_AIN_GND = 5,
+    ADS1015_MUX_AIN2_AIN_GND = 6,
+    ADS1015_MUX_AIN3_AIN_GND = 7,
+
+} ads1015_mux_t;
+
+typedef enum ads1015_pga_e{
+    ADS1015_PGA_6_144 = 0,
+    ADS1015_PGA_4_096 = 1,
+    ADS1015_PGA_2_048 = 2,
+    ADS1015_PGA_1_024 = 3,
+    ADS1015_PGA_0_512 = 4,
+    ADS1015_PGA_0_256 = 5,
+    ADS1015_PGA_0_256 = 6,
+    ADS1015_PGA_0_256 = 7,
+
+} ads1015_pga_t;
+
+typedef enum ads1015_mode_e{
+    ADS1015_MODE_CONTINUOUS  = 0,
+    ADS1015_MODE_SINGLE_SHOT = 1,
+
+} ads1015_mode_t;
+
+typedef enum ads1015_data_rate_e{
+    ADS1015_DATA_RATE_128SPS  = 0,
+    ADS1015_DATA_RATE_250SPS  = 1,
+    ADS1015_DATA_RATE_490SPS  = 2,
+    ADS1015_DATA_RATE_920SPS  = 3,
+    ADS1015_DATA_RATE_1600SPS = 4,
+    ADS1015_DATA_RATE_2400SPS = 5,
+    ADS1015_DATA_RATE_3300SPS = 6,
+    ADS1015_DATA_RATE_3300SPS = 7,
+
+} ads1015_data_rate_t;
+
+typedef enum ads1015_comp_mode_e{
+    ADS1015_COMP_MODE_TRADITIONAL = 0,
+    ADS1015_COMP_MODE_WINDOW      = 1,
+
+} ads1015_comp_mode_t;
+
+typedef enum ads1015_comp_pol_e{
+    ADS1015_COMP_POL_LOW  = 0,
+    ADS1015_COMP_POL_HIGH = 1,
+
+} ads1015_comp_pol_t;
+
+typedef enum ads1015_comp_lat_e{
+    ADS1015_COMP_POL_LOW  = 0,
+    ADS1015_COMP_POL_HIGH = 1,
+
+} ads1015_comp_lat_t;
 
 #endif
